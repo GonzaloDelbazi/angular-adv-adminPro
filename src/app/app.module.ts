c18781d826d878cf583fe875099ac921b1f96046
Gonzalo <gonzalodelbazi@gmail.com>
2021-04-05 10:18:42 -0300

    fin seccion 4 - Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,40 +2,36 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 // ====================================================================================
-// RUTAS
+// MODULOS
 // ====================================================================================
 import { AppRoutingModule } from './app-routing.module';
+import { PagesModule } from './pages/pages.module';
+import { SharedModule } from './shared/shared.module';
 
-
+// ====================================================================================
+// COMPONENTES
+// ====================================================================================
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { NotPageFoundComponent } from './pages/not-page-found/not-page-found.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { BreadCrumbsComponent } from './shared/bread-crumbs/bread-crumbs.component';
-import { SideBarComponent } from './shared/side-bar/side-bar.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { PagesComponent } from './pages/pages.component';
+import { NotPageFoundComponent } from './not-page-found/not-page-found.component';
+import { AuthModule } from './auth/auth.module';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
     NotPageFoundComponent,
-    DashboardComponent,
-    BreadCrumbsComponent,
-    SideBarComponent,
-    HeaderComponent,
-    ProgressComponent,
-    Grafica1Component,
-    PagesComponent
+    
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule,
+    SharedModule,
+    AuthModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
